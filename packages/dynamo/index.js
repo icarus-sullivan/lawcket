3e52b504c60486ffa0dae37d7d265bc1d6748689
Chris Sullivan <chrissullivan.dev@gmail.com>
2019-02-03 17:50:37 -0500

    More dynamo changes, adding dry run

--- packages/dynamo/index.js
+++ packages/dynamo/index.js
@@ -2,32 +2,64 @@ const AWS = require('aws-sdk');
 const doc = new AWS.DynamoDB.DocumentClient();
 
 const DEFAULT_OPTIONS = {
+  tableName: '',
   sync: false,
+  dry: false,
 };
 
-const createDynamo = (opts = DEFAULT_OPTIONS) => async (requestContext) => {
-  const merged = { ...DEFAULT_OPTIONS, ...opts };
-  if (!merged || !merged.tableName) {
+const createRequest = ({ tableName, connectionId, domainName, stage }) => ({
+  TableName: tableName,
+  Item: {
+    connectionId,
+    domainName,
+    stage,
+  },
+});
+
+const baseRequest = ({ tableName, connectionId }) => ({ 
+  TableName: tableName, 
+  Key: { connectionId } 
+});
+
+module.exports = (opts = DEFAULT_OPTIONS) => async (requestContext) => {
+  const options = { ...DEFAULT_OPTIONS, ...opts };
+  if (!options || !options.tableName) {
     throw new Error('Must provide a tableName to sync clients with');
   }
 
-  if (!merged.sync) {
+  if (!options.sync) {
     return requestContext;
   }
 
-  const { tableName } = merged;
-  const { connectionId, domainName, stage, eventType } = requestContext;
-  switch(eventType) {
+  const { tableName } = options;
+  const mergedRequest = { ...requestContext, tableName };
+  switch(mergedRequest.eventType) {
     case 'CONNECT': {
-      await doc.put({ TableName: tableName, Item: { connectionId, domainName, stage } }).promise();
-      return requestContext;
+      const request = createRequest(mergedRequest);
+      if (options.dry) {
+        console.log('Adding connection', request);
+        break;
+      }
+      console.log(doc.put);
+      await doc.put(request).promise();
+      break;
     }
     case 'DISCONNECT': {
-      await doc.delete({ TableName: tableName, Key: connectionId }).promise();
-      return requestContext;
+      const request = baseRequest(mergedRequest);
+      if (options.dry) {
+        console.log('Removing connection', request);
+        break;
+      }
+      await doc.delete(request).promise();
+      break;
     }
     case 'MESSAGE': {
-      const restored = await doc.get({ TableName: tableName, Key: connectionId }).promise();
+      const request = baseRequest(mergedRequest);
+      if (options.dry) {
+        console.log('Restoring connection', request);
+        break;
+      }
+      const restored = await doc.get(request).promise();
       return restored && restored.Item 
         ? restored.Item 
         : requestContext;
@@ -36,5 +68,3 @@ const createDynamo = (opts = DEFAULT_OPTIONS) => async (requestContext) => {
 
   return requestContext;
 };
-
-module.exports = createDynamo;
